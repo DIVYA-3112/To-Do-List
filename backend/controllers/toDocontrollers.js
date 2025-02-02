fba141be737e1dd155f639bf46c1c0b364a2ff95
DIVYA-3112 <113194039+DIVYA-3112@users.noreply.github.com>
2025-02-02 17:20:00 +0530

    Final Commit

--- backend/controllers/toDocontrollers.js
+++ backend/controllers/toDocontrollers.js
@@ -11,10 +11,11 @@ const getToDos = async (req, res) => {
 const saveToDos = async (req, res) => {
     try
     {
-        const { toDo } = req.body;
+        const {toDo}  = req.body;
+        console.log(req.body);
         if(!toDo || typeof toDo !== "string")
         {
-            return res.status(400).json({ error: "Invalid task format. Expected a string." });
+            return res.status(200).json({ error: "Invalid task format. Expected a string." });
         }
         const newTask = await toDoModel.create({ toDo: toDo }); // Pass a string
         console.log("Task saved successfully");
@@ -23,4 +24,34 @@ const saveToDos = async (req, res) => {
     catch(err) {console.log(err, "Error in saving the Task")};
 }
 
-module.exports = {getToDos, saveToDos};
+
+const updateToDos = async (req,res) => {
+    try
+    {
+        const {id} = req.params; 
+        console.log(id);
+        const {toDo} = req.body;
+
+        const updatedTask = await toDoModel.findByIdAndUpdate(id, {toDo}, {new : true});
+
+        if(!updatedTask) return res.status(404).json({message : "Task not found"});
+        res.status(200).json({message : "task updated successfully", updatedTask});
+        // toDoModel.findByIdAndUpdate({id}, {toDo});
+    }
+    catch(err) {console.log(err, "unable to update Task")};
+}
+
+const deleteToDos = async (req, res) => {
+    try{
+
+        const {id} = req.params;
+        const findTask = await toDoModel.findOne({_id : id});
+        if(!findTask) return res.status(404).json({message : `task with id - ${id} does not exist`});
+        const deleteTask = await toDoModel.findByIdAndDelete(id);
+        if(!deleteTask) return res.status(404).json("Task delete failed");
+        res.status(200).json({message : "task deleted successfully", deleteTask});
+    }
+    catch (err) {console.log(err, "unable to delete task")};
+};
+
+module.exports = {getToDos, saveToDos, updateToDos, deleteToDos};
--- backend/routes/toDoRoutes.js
+++ backend/routes/toDoRoutes.js
@@ -1,8 +1,10 @@
 const Router = require("express");
 const router = Router();
-const {getToDos, saveToDos} = require("../controllers/toDocontrollers");
+const {getToDos, saveToDos, updateToDos, deleteToDos} = require("../controllers/toDocontrollers");
 
 router.get("/get", getToDos);
 router.post("/save", saveToDos);
+router.put("/update/:id", updateToDos);
+router.delete("/delete/:id", deleteToDos);
 
 module.exports = router;
